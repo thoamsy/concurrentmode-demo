3ca1e0a302d11044201ccc8d7e79c290c338f6ae
YangKui <c6swift@gmail.com>
2018-11-09 22:56:41 +0800

    fix: 修复设置了 opacity，但是还是可以点击的问题

--- src/Tab.jsx
+++ src/Tab.jsx
@@ -42,21 +42,24 @@ const Tab = props => {
       </div>
       <Suspense fallback="🌀">
         <div style={{ position: 'relative' }}>
-          {loadedTab.map((tab, i) => (
-            <section
-              key={i}
-              className="content"
-              style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                opacity: +(current === i),
-              }}
-            >
-              {tab.props.children}
-            </section>
-          ))}
+          {loadedTab.map((tab, i) => {
+            return (
+              <section
+                key={i}
+                className="content"
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  opacity: +(current === i),
+                  pointerEvents: current === i ? 'auto' : 'none',
+                }}
+              >
+                {tab.props.children}
+              </section>
+            );
+          })}
         </div>
       </Suspense>
     </>
