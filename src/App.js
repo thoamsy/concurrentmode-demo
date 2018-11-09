28e05ca9348b896db0cc18f73b2532c1e03bb045
YangKui <c6swift@gmail.com>
2018-11-09 22:12:34 +0800

    refactor: class to function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { Component, Suspense, lazy, createContext } from 'react';
+import React, { Suspense, lazy, createContext } from 'react';
 import styled, { css } from 'styled-components';
 
 import Tab from './Tab';
 import Text from './panel/Text';
-import lazyload from './lazyLoad';
-const Review = lazyload(() => import('./panel/Review'));
-const Form = lazyload(() => import('./panel/Form'));
+const Review = lazy(() => import('./panel/Review'));
+const Form = lazy(() => import('./panel/Form'));
 
 const color = css`
   & .tabs li.is-active a {
@@ -20,31 +19,27 @@ const { Panel } = Tab;
 
 export const WhichMode = createContext(null);
 
-class App extends Component {
-  render() {
-    return (
-      <Container mode={this.props.mode}>
-        <WhichMode.Provider value={this.props.mode}>
-          <h1 className="title">
-            {this.props.mode === 1 ? 'Current' : 'Sync'} Mode
-          </h1>
-          <Tab>
-            <Panel labelName="Tab 1">
-              <Text />
-            </Panel>
-            <Panel labelName="Tab 2">
-              <Suspense fallback="Loading…" maxDuration={300}>
-                <Form />
-              </Suspense>
-            </Panel>
-            <Panel labelName="Tab 3">
-              <Review />
-            </Panel>
-          </Tab>
-        </WhichMode.Provider>
-      </Container>
-    );
-  }
-}
+const App = ({ mode }) => {
+  return (
+    <Container mode={mode}>
+      <WhichMode.Provider value={mode}>
+        <h1 className="title">{mode === 1 ? 'Current' : 'Sync'} Mode</h1>
+        <Tab>
+          <Panel labelName="Tab 1">
+            <Text />
+          </Panel>
+          <Panel labelName="Tab 2">
+            <Suspense fallback="Loading…" maxDuration={300}>
+              <Form />
+            </Suspense>
+          </Panel>
+          <Panel labelName="Tab 3">
+            <Review />
+          </Panel>
+        </Tab>
+      </WhichMode.Provider>
+    </Container>
+  );
+};
 
 export default App;
